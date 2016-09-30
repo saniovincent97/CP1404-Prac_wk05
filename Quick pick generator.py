f8c3c6f98ab16bd92e95aec87727f1df708aea5b
saniovincent97 <svinc26@hotmail.com>
2016-09-30 13:06:19 +1000

    Update to last few weeks of work

--- Quick pick generator.py
+++ Quick pick generator.py
@@ -3,15 +3,14 @@ import random
 NUMBERS_IN_ROW = 6
 LOWER = 1
 MAX = 45
-number_generator = []
 quick_picks = int(input("How many quick picks would you like to generate?"))
-for quick_picks in range(quick_picks):
+for number in range(quick_picks):
+    numbers = []
     for num in range(NUMBERS_IN_ROW):
         random_number = random.randint(LOWER,MAX)
-        while random_number in number_generator:
+        while random_number in numbers:
             random_number = random.randint(LOWER, MAX)
-        number_generator.append(random_number)
-    number_generator.sort()
-    print(number_generator)
-    number_generator.clear()
+        numbers.append(random_number)
+    numbers.sort()
+
 
--- number_list.py
+++ number_list.py
@@ -4,7 +4,7 @@ for i in range(5):
     input_number = int(input("Number:"))
     numbers.append(input_number)
 print("The first number is {}".format(numbers[0]))
-print("The last number is {}".format(numbers[4]))
+print("The last number is {}".format(numbers[-1]))
 print("The smallest number is {}".format(min(numbers)))
 print("The largest number is {}".format(max(numbers)))
 average = sum(numbers)/len(numbers)
